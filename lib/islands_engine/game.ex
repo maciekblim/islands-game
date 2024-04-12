c7c5e56b82387d4f76a2b6e3654702628ff80cce
Maciej Blim <maciej.blim@vistex.com>
2024-04-12 08:49:13 +0200

    experimenting with GenServer

---
+++ lib/islands_engine/game.ex
@@ -0,0 +1,24 @@
+defmodule IslandsEngie.Game do
+    use GenServer
+
+    def handle_info(:first, state) do
+        IO.puts("This message has been handled by handle_info/2, matching on :first")
+        {:noreply, state}
+    end
+
+    def handle_cast({:demo_cast, new_value}, state) do
+        {:noreply, Map.put(state, :test, new_value)}
+    end
+
+    def handle_call(:demo_call, _from, state) do
+        {:reply, state, state}
+    end
+
+    def demo_cast(game, new_value) do
+        GenServer.cast(game, {:demo_cast, new_value})
+    end
+
+    def demo_call(game) do
+        GenServer.call(game, :demo_call)
+    end
+end
