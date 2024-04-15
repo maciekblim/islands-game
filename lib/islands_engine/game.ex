62983bec2e7f88886b9ba9ee6a7932eb6937b7d8
Maciej Blim <maciej.blim@vistex.com>
2024-04-15 11:03:19 +0200

    guessing command

--- lib/islands_engine/game.ex
+++ lib/islands_engine/game.ex
@@ -68,6 +68,26 @@ defmodule IslandsEngie.Game do
         end
     end
 
+    def handle_call({:guess_coordinate, player, row, col}, _from, state) do
+        oponent_player = opponent(player)
+        opponent_board = player_board(state, oponent_player)
+
+        with {:ok, rules} <- Rules.check(state.rules, {:guess_coordinate, player}),
+             {:ok, coordinate} <- Coordinate.new(row, col),
+             {hit_or_miss, forested_island, win_status, opponent_board} <- Board.guess(opponent_board, coordinate),
+             {:ok, rules} <- Rules.check(rules, {:win_check, win_status})
+        do
+            state
+            |> update_board(oponent_player, opponent_board)
+            |> update_guesses(player, hit_or_miss, coordinate)
+            |> update_rules(rules)
+            |> reply_success({hit_or_miss, forested_island, win_status})
+        else
+            :error -> {:reply, :error, state}
+            {:error, :invalid_coordinate} -> {:reply, {:error, :invalid_coordinate}, state}
+        end
+    end
+
     def start_link(name) when is_binary(name) do
         GenServer.start_link(__MODULE__, name, [])
     end
@@ -127,6 +147,18 @@ defmodule IslandsEngie.Game do
         GenServer.call(game, {:set_islands, player})
     end
 
+    @doc """
+    ## Examples
+
+        iex> {:ok, game} = IslandsEngie.Game.start_link("Miles")
+        ...> IslandsEngie.Game.add_player(game, "Trane")
+        ...> IslandsEngie.Game.guess_coordinate(game, :player1, 1, 1)
+        :error
+    """
+    def guess_coordinate(game, player, row, col) when player in @players do
+        GenServer.call(game, {:guess_coordinate, player, row, col})
+    end
+
     defp update_player2_name(state, name),
         do: put_in(state.player2.name, name)
 
@@ -136,9 +168,18 @@ defmodule IslandsEngie.Game do
     defp update_board(state, player, board),
         do: Map.update!(state, player, fn player -> %{player | board: board} end)
 
+    defp update_guesses(state, player, hit_or_miss, coordinate) do
+        update_in(state[player].guesses, fn guesses ->
+           Guesses.add(guesses,hit_or_miss, coordinate)
+        end)
+    end
+
     defp reply_success(state, reply),
         do: {:reply, reply, state}
 
     defp player_board(state, player),
         do: Map.get(state, player).board
+
+    defp opponent(:player1), do: :player2
+    defp opponent(:player2), do: :player1
 end
