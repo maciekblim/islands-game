fe50805ee71f2420403beadeacfac159efbd14b8
Maciej Blim <maciej.blim@vistex.com>
2024-04-17 15:51:05 +0200

    replacing depricated Supervisor with DynamicSupervisor

--- lib/islands_engine/game_supervisor.ex
+++ lib/islands_engine/game_supervisor.ex
@@ -1,23 +1,23 @@
 defmodule IslandsEngine.GameSupervisor do
-  # TODO check DynamicSupervisor https://hexdocs.pm/elixir/1.13/DynamicSupervisor.html
-  use Supervisor
+  use DynamicSupervisor
 
   alias IslandsEngine.Game
 
-  def start_link(_) do
-    Supervisor.start_link(__MODULE__, :ok, name: __MODULE__)
+  def start_link(_init_arg) do
+    DynamicSupervisor.start_link(__MODULE__, :ok, name: __MODULE__)
   end
 
   def init(:ok) do
-    Supervisor.init([Game], strategy: :simple_one_for_one)
+    DynamicSupervisor.init(strategy: :one_for_one)
   end
 
   def start_game(name) do
-    Supervisor.start_child(__MODULE__, [name])
+    spec = %{id: Game, start: {Game, :start_link, [name]}}
+    DynamicSupervisor.start_child(__MODULE__, spec)
   end
 
   def stop_game(name) do
-    Supervisor.terminate_child(__MODULE__, pid_from_name(name))
+    DynamicSupervisor.terminate_child(__MODULE__, pid_from_name(name))
   end
 
   defp pid_from_name(name) do
@@ -26,24 +26,3 @@ defmodule IslandsEngine.GameSupervisor do
     |> GenServer.whereis()
   end
 end
-
-# defmodule IslandsEngine.GameSupervisor do
-#   use DynamicSupervisor
-
-#   alias IslandsEngine.Game
-
-#   def start_link() do
-#     DynamicSupervisor.start_link(__MODULE__, :ok, name: __MODULE__)
-#   end
-
-#   def start_child() do
-#     spec = {Game, []}
-#     DynamicSupervisor.start_child(__MODULE__, spec)
-#   end
-
-#   def init(:ok) do
-#     DynamicSupervisor.init(
-#       strategy: :one_for_one
-#     )
-#   end
-# end
